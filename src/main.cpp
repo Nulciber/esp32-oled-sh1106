eed6c20f668f8b00b92833c566469dfa25aa6e35
Nulciber <Nulciber@protonmail.com>
2026-05-30 14:56:16 +0200

    eneide 125

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,9 @@
 #include "txt_eneide.h"
 #include "txt_aveMaria.h"
 
-
 #define SDA_PIN 8
 #define SCL_PIN 9
-#define TEXTEQUIDEFILE txt_aveMaria
+#define TEXTEQUIDEFILE txt_eneide
 
 void setup()
 {
@@ -17,9 +16,8 @@ void setup()
     sh1106_init();
     clear();
     display();
-     draw_char(10, 10, 0x61);    // a — dans police_standard.h
-    draw_char(20, 10, 0x3B1);   // α — dans police_accents.h
-    draw_char(30, 10, 0xE000);  // glyphe — dans police_accents.h
+    draw_string(0, 10, "éèëêēàäâāïîöôùü");
+    draw_string(0, 20, "Éœç");
     display();
 }
 
@@ -27,7 +25,7 @@ void loop()
 {
     int longueur = strlen(TEXTEQUIDEFILE) * 8;
     static int i = 0;
-    
+
     clear();
     draw_string(WIDTH - i, 28, TEXTEQUIDEFILE);
     if (i > longueur - WIDTH)
@@ -35,5 +33,6 @@ void loop()
     display();
     delay(1);
     i++;
-    if (i >= longueur) i = 0;
+    if (i >= longueur)
+        i = 0;
 }
